b6303585cb7b869f9162c3efb7d3e83d560f2703
Michael <macorlett@gmail.com>
2014-11-27 20:02:52 +1300

    font changes

--- js/editor.js
+++ js/editor.js
@@ -77,7 +77,7 @@ $(document).on('focusout','.creator--input',function(){
   }
   if($(this).val()){
     createListItem($(this).val());
-    $(this).parent().append('<textarea class="creator--input" placeholder="Add another item"></textarea>');
+    $(this).parent().append('<textarea class="creator--input" placeholder="Add another item :)"></textarea>');
     $(this).remove();
   }
 });
